0a04727fa763f8d1fc7791ed6432d8079113eff9
Artemy Erkov <artemyerkov@gmail.com>
2025-05-08 00:12:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 print("Hello")
 print("chlen")
+
+# to commit changes
+# git add .
+# git commit -m "<message>"
+# git push
+
+# to fetch changes from remote repository
+# git pull
+
+# to create new code branch and change to it
+# git checkout -b name-of-branch
+
+# to change branch
+# git checkout name-of-branch
+
+# to fetch all remote branches
+# git fetch
+
+# don't make changes until assuring pulling remote code (there may be new code), in case if you do run these commands
+# git stash
+# git pull
+# git stash pop
